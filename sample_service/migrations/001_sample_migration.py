3aebcda2660ac8646457c804f699781d804b6f0d
Jalen <jalen.gill@macaulay.cuny.edu>
2023-01-05 13:40:32 -0500

    Created starter files to add routers and queries to Fast API

--- sample_service/migrations/001_sample_migration.py
+++ sample_service/migrations/001_sample_migration.py
@@ -2,37 +2,65 @@ steps = [
     [
         # "Up" SQL statement
         """
-        CREATE TABLE dummy (
+        CREATE TABLE user (
             id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
+            username VARCHAR(20) NOT NULL UNIQUE,
+            password VARCHAR(20) NOT NULL,
+            email VARCHAR(50) NOT NULL UNIQUE,
+            first_name VARCHAR(50) NOT NULL,
+            last_name VARCHAR(50) NOT NULL
         );
         """,
         # "Down" SQL statement
         """
-        DROP TABLE dummy;
-        """
+        DROP TABLE user;
+        """,
     ],
     [
         # "Up" SQL statement
         """
-        CREATE TABLE big_dummy (
+        CREATE TABLE movie (
             id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
+            title VARCHAR(255) NOT NULL,
+            released DATE NOT NULL,
+            plot TEXT NOT NULL,
+            rated VARCHAR(5) NOT NULL,
+            imdbID VARCHAR(9) NOT NULL,
+            poster VARCHAR(1000) NOT NULL
         );
         """,
         # "Down" SQL statement
         """
-        DROP TABLE big_dummy;
+        DROP TABLE movie;
+        """,
+    ],
+    [
+        # "Up" SQL statement
         """
-    ]
+        CREATE TABLE movie_item (
+            id SERIAL PRIMARY KEY NOT NULL,
+            movie_id INTEGER REFERENCES movie(id),
+            movie_list_id INTEGER REFERENCES movie_list(id)
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE movie_item;
+        """,
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE TABLE movie_list (
+            id SERIAL PRIMARY KEY NOT NULL,
+            name VARCHAR(100) NOT NULL,
+            owner INTEGER REFERENCES user(id),
+
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE movie_list;
+        """,
+    ],
 ]
