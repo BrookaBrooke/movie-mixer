2821d9553e2a7a0ed5be1a91db01fcb4900c6583
Michel Menin <msmenin@gmail.com>
2023-01-06 19:13:31 -0400

    movie group changes

--- sample_service/queries/movie_group.py
+++ sample_service/queries/movie_group.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from typing import Dict, Any
 from queries.pool import pool
+from typing import List
 
 
 class MovieGroup(BaseModel):
@@ -27,22 +28,28 @@ class MovieGroupRepository:
                 db.execute("SELECT * FROM movie_groups")
                 return db.fetchall()
 
-    def get(self, id: int) -> Dict[str, Any]:
+    def get(self, id: int) -> MovieGroupOut:
         with pool.connection() as conn:
             with conn.cursor() as db:
-                db.execute("SELECT * FROM movie_groups WHERE id = %s", (id,))
-                return db.fetchone()
+                db.execute("SELECT * FROM movie_groups WHERE id = %s", (id))
+                print("DB : ",db)
+                data = db.fetchone()
+                print(data)
+                return MovieGroupOut(**data)
 
-    def create(self, movie_group: MovieGroupIn) -> Dict[str, Any]:
+    def create(self, movie_group: MovieGroupIn) -> MovieGroupOut:
         with pool.connection() as conn:
             with conn.cursor() as db:
                 db.execute(
                     "INSERT INTO movie_groups (name, owner) VALUES (%s, %s) RETURNING id",
                     (movie_group.name, movie_group.owner),
                 )
-                movie_group_dict = movie_group.dict()
-                movie_group_dict["id"] = db.fetchone()[0]
-                return movie_group_dict
+                print(movie_group)
+                data = movie_group.dict()
+                print(data)
+                id = db.fetchone()[0]
+                print("ID: ",id)
+                return MovieGroupOut(id=id, **data)
 
     def update(self, id: int, movie_group: MovieGroupIn) -> Dict[str, Any]:
         with pool.connection() as conn:
