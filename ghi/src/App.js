38a1618ae821e26ff533ec5a1c9b43ca56ed77b5
Brooke Vonderheid <bevfrisco@gmail.com>
2023-01-10 16:00:00 -0500

    workign on accounts

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -10,34 +10,7 @@ import MovieGroups from "./components/MovieGroups";
 import MovieGroupForm from "./components/MovieGroupForm";
 import MovieSearch from "./components/MovieSearch";
 
-<<<<<<< HEAD
-function App() {
-  const [launch_info, setLaunchInfo] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
-      console.log('fastapi url: ', url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
-
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
-    }
-    getData();
-  }, [])
-
-
-=======
 function App(props) {
->>>>>>> main
   return (
     <BrowserRouter>
       <Nav />
--- ghi/src/components/AccountForm.js
+++ ghi/src/components/AccountForm.js
@@ -1,5 +1,32 @@
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useLogInMutation } from "../store/authApi";
+import { updateField } from "../store/accountSlice";
+import { useNavigate } from "react-router-dom";
+import React, { useEffect } from "react";
+
 function AccountForm(props) {
-  return "Form here";
+  const dispatch = useDispatch();
+  const { username, password } = useSelector((state) => state.account);
+  const [
+    logIn,
+    { error, isloading: logInLoading, isSuccess: loginSuccessful },
+  ] = useLogInMutation();
+  const field = useCallback(
+    (e) =>
+      dispatch(updateField({ field: e.target.name, value: e.target.value })),
+    [dispatch]
+  );
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    if (loginSuccessful) {
+      navigate("/");
+    }
+  };
+  useEffect(() => {
+    handleLogin();
+  });
 }
 
 export default AccountForm;
