88d06a8aa7f009c2c27624177fac08bf9e0dfee0
bntg01 <bntgion@gmail.com>
2023-01-17 17:44:07 -0500

    Add test for movie_groups

--- sample_service/tests/test_movie_groups.py
+++ sample_service/tests/test_movie_groups.py
@@ -1,13 +1,55 @@
 import json
 from fastapi.testclient import TestClient
 from typing import List
-from queries.movies import MovieRepository
-from routers.movies import MovieIn, MovieOut
+from queries.movie_group import MovieGroupRepository
+from routers.movie_group import MovieGroupIn, MovieGroupOut
 from authenticator import authenticator
 from main import app
 
 client = TestClient(app=app)
 
 
-def get_current_account_data_mock():
-    return {"id": 17, "username": "kramer"}
+class MovieGroupRepositoryMock:
+    def create(self, movie_group: MovieGroupIn) -> MovieGroupOut:
+        movie_group_dict = movie_group.dict()
+        return MovieGroupOut(id=1, **movie_group_dict)
+
+    def get(self) -> List[MovieGroupOut]:
+        return []
+
+    def get_one(self, id) -> MovieGroupOut:
+        return id
+
+
+def test_create_movie_group():
+    # Arrange
+    app.dependency_overrides[MovieGroupRepository] = MovieGroupRepositoryMock
+    movie_group_body = {
+        "id": 1,
+        "name": "group",
+        "owner": 1,
+    }
+
+    # Act
+    res = client.post("/movie-groups", json=movie_group_body)
+
+    # Assert
+    assert res.status_code == 200
+
+    # A cleanup
+    app.dependency_overrides = {}
+
+
+def test_get_movie_groups():
+    # Arrange
+    app.dependency_overrides[MovieGroupRepository] = MovieGroupRepositoryMock
+
+    # Act
+    res = client.get("/movie-groups")
+
+    # Assert
+    assert res.status_code == 200
+    assert res.json() == []
+
+    # A cleanup
+    app.dependency_overrides = {}
