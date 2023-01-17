eda543fdc63df432d1e89658be5a9b467c51475c
Jalen <jalen.gill@macaulay.cuny.edu>
2023-01-17 12:21:39 -0500

    Some unit tests added

--- sample_service/migrations/001_sample_migration.py
+++ sample_service/migrations/001_sample_migration.py
@@ -17,7 +17,7 @@ steps = [
     [
         """
         CREATE TABLE movies (
-            id SERIAL PRIMARY KEY NOT NULL,
+            id SERIAL PRIMARY KEY,
             title VARCHAR(255) NOT NULL,
             release_date DATE NOT NULL,
             overview TEXT NOT NULL,
