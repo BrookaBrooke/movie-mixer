5d951fc503fc9b855807af9dde01eb9a19d629eb
Jalen <jalen.gill@macaulay.cuny.edu>
2023-01-19 13:08:06 -0500

    Edited main page

--- ghi/src/components/MainPage.js
+++ ghi/src/components/MainPage.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {Carousel} from 'react-bootstrap'
+import { Carousel } from "react-bootstrap";
 import ReactPlayer from "react-player";
 // import "./VideoCarousel.css";
-
 
 function MainPage() {
   const navigate = useNavigate();
@@ -22,65 +21,78 @@ function MainPage() {
     {
       id: 76600,
       title: "Avatar: The Way of Water",
-      src: (`https://youtu.be/d9MyW72ELq0`),
-      credit: "20th Century / 20th Century Studios"
+      src: `https://youtu.be/d9MyW72ELq0`,
+      credit: "20th Century / 20th Century Studios",
     },
-  ]
-
-
+  ];
 
   return (
+    <div className="banner-search text-light">
       <div>
-        <div className="home-pic">
-          <Carousel>
-            {videoProperties.map((videoObj) => {
-              return (
-                <Carousel.Item key={videoObj.id}>
-                  <ReactPlayer
+        <h1 className="home-header">MovieMixer</h1>
+        <p className="text-center">
+          Find information for any movie you desire and organize them into lists
+          to keep track of upcoming titles or old favorites!
+        </p>
+      </div>
+      <div>
+        <div className="pb-3 d-flex justify-content-center">
+          <div className="p-2">
+            {" "}
+            <h2 className="text-center">Search Movies</h2>
+            <form
+              className="d-flex container"
+              role="search"
+              onSubmit={redirectToSearchPage}
+            >
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                required
+                onChange={onChange}
+              />
+              <button className="btn btn-outline-danger" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+          <div className="text-center p-2">
+            <p>Log in to get started!</p>
+            <button className="btn btn-outline-primary">Log in</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="">
+        <Carousel>
+          {videoProperties.map((videoObj) => {
+            return (
+              <Carousel.Item key={videoObj.id}>
+                <ReactPlayer
                   url={videoObj.src}
                   playing={true}
                   width="100%"
-                  height="1080px"
+                  height="540px"
                   pip={true}
                   // fullscreen={true}
                   muted={true}
                   // hover={true}
                   // controls={true}
-
-
-                  />
-                  <Carousel.Caption>
-                    <h2>New Release</h2>
-                    <h3>{videoObj.title}</h3>
-                    <p>Credits: {videoObj.credit}</p>
-                    {/* <h4>Add to your Favorites</h4> */}
-                  </Carousel.Caption>
-                </Carousel.Item>
-              )
-            })}
-          </Carousel>
-          </div>
-          <div className='main-page-search'>
-          <h2 className="home-header">Search Movies</h2>
-          <form
-            className="d-flex container"
-            role="search"
-            onSubmit={redirectToSearchPage}
-          >
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-              required
-              onChange={onChange}
-            />
-            <button className="btn btn-outline-danger" type="submit">
-              Search
-            </button>
-          </form>
-        </div>
+                />
+                <Carousel.Caption>
+                  <h2>New Release</h2>
+                  <h3>{videoObj.title}</h3>
+                  <p>Credits: {videoObj.credit}</p>
+                  {/* <h4>Add to your Favorites</h4> */}
+                </Carousel.Caption>
+              </Carousel.Item>
+            );
+          })}
+        </Carousel>
       </div>
+    </div>
   );
 }
 
