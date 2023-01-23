279fcc49a734c188548016bbf1a2b4f26abd5732
Brooke Vonderheid <bevfrisco@gmail.com>
2023-01-23 17:31:26 -0500

    working on tests

--- sample_service/tests/test_3rd_party_api.py
+++ sample_service/tests/test_3rd_party_api.py
@@ -1,13 +1,23 @@
 from fastapi.testclient import TestClient
+from queries.api_movies import ApiMovieQueries
+from routers.movies import MovieIn, MovieOut
 from main import app
+from typing import List
+
 
 client = TestClient(app=app)
 
 
-def test_get_api():
-    # Arrange
+class MockAPIQuery:
+    def get(self) -> List[MovieOut]:
+        return []
 
+
+def test_search_movies():
+    # Arrange
+    app.dependency_overrides[ApiMovieQueries] = MockAPIQuery()
     # Act
-    res = client.get('/api/')
+    res = client.get('/api-movies/search/{title}')
     # Assert
     assert res.status_code == 200
+    assert res.json() == []
