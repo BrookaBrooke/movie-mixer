34c51e094ff8b10c5f860b6f9c63227b4eb21322
Jalen <jalen.gill@macaulay.cuny.edu>
2023-01-27 18:17:57 -0500

    small edits

--- sample_service/tests/test_3rd_party_api.py
+++ sample_service/tests/test_3rd_party_api.py
@@ -1,36 +1,23 @@
 from fastapi.testclient import TestClient
 from queries.api_movies import ApiMovieQueries
-from routers.movies import MovieIn, MovieOut
 from main import app
-from typing import List
 
 
 client = TestClient(app=app)
 
 
 class MockAPIQuery:
-    def getmovies(self):
-        return []
+    def get_one_movie(self, id: str) -> dict:
+        return {"movie_id": id}
 
 
-def test_get_movies():
+def test_get_movie():
     # Arrange
-    app.dependency_overrides[ApiMovieQueries] = MockAPIQuery()
+    app.dependency_overrides[ApiMovieQueries] = MockAPIQuery
     # Act
-    res = client.get('/api-movies/detail{id}')
+    res = client.get("/api-movies/detail/1")
     # Assert
     assert res.status_code == 200
-    assert res.json() == {
-        "detail": [
-            {
-                "loc": [
-                    "string",
-                    0
-                ],
-                "msg": "string",
-                "type": "string"
-            }
-        ]
-    }
+    assert res.json()["movie_id"] == "1"
 
     app.dependency_overrides = {}
